4709bdbbc7f03336187fa779df52f6674a660f2c
jieun246 <idgocm@naver.com>
2021-06-03 22:01:05 +0900

    챕터 5 완료

--- src/test/java/com/test/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/test/springboot/web/PostsApiControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -41,6 +42,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void register() throws Exception{
         String title = "title";
         String content = "content";
@@ -63,6 +65,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void update() throws Exception{
         Posts savedPosts = postsRepository.save(Posts.builder()
         .title("title").content("content").author("author").build());
